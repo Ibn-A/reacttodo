c0a8d068f4ca9f01ffbbffc4baec59fdb56be175
Ibn-A <ibn_ali@yahoo.com>
2021-04-23 11:37:24 +0200

    chek & detele features : - setup deleteHandler function pour supprimer les taches. - setup completeHandler function pour marquer les taches complété.

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -3,13 +3,20 @@ import React from 'react'
 //import Components
 import Todo from './Todo';
 
-const TodoList = ({todos}) => {
+const TodoList = ({todos, setTodos}) => {
 
     return (
         <div className="todo-container">
             <ul className="todo-list">
-                {todos.map(todo => (
-                    <Todo key={todo.id} text={todo.text} id={todo.id} />
+                {todos.map((todo) => (
+                    <Todo
+                        setTodos={setTodos} 
+                        todos={todos} 
+                        key={todo.id} 
+                        text={todo.text}
+                        todo={todo} 
+                        id={todo.id} 
+                    />
                 ))}
             </ul>
         </div>
