8dbb0c088c7235864fe5a71ca48d4b9c36b7fba5
Ibn-A <ibn_ali@yahoo.com>
2021-06-05 00:44:56 +0200

    Update Dropdown menu : - selection option du menu deroulant et affichage.

--- src/components/DropdownMenu.js
+++ src/components/DropdownMenu.js
@@ -4,7 +4,19 @@ import useDetectOutsideClick from './useDetectOutsideClick';
 const DropdownMenu = () => {
     const dropdownRef = useRef(null);
     const [isActive, setIsActive] = useDetectOutsideClick(dropdownRef, false);
-    const onClick = () => setIsActive(!isActive);
+    const onClick = () => {
+        setIsActive(!isActive);
+        /** Acceder au DOM en direct n'est pas une bonne pratique, donc a améliorer!! */
+        for(const option of document.querySelectorAll(".custom-option")) {
+            option.addEventListener('click', function() {
+                if (!this.classList.contains('selected')) {
+                    this.parentNode.querySelector('.custom-option.selected').classList.remove('selected');
+                    this.classList.add('selected');
+                    this.closest('.custom-select').querySelector('.custom-select__trigger span').textContent = this.textContent;
+                }
+            })
+        }
+    }
     
     return (
         <div className="custom-select-wrapper">
